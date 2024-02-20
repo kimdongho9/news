2ce52d720d83e9f3d83ede82fafca67742fdd546
kimdongho9 <guzzi9393@gamil.com>
2024-02-21 00:29:46 +0900

    add

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 // const API_KEY = `f92a158943c1483e814a7bf93bce3bbb`;
-const API_KEY = `f92a158943c1483e814a7bf93bce3bbb`;
 
 
+
+//async 사용해야 await 사용가능
 const getLatestNews = async () => {
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
 
+
+
+  //과제용
+  const url = new URL(`https://celadon-zabaione-c1d562.netlify.app/top-headlines?q=${keyword}`);
+
+  //url api 가져옴
   const response = await fetch(url);
   const data = await response.json()
 
